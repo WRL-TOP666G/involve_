f53460d2d2dbd33a821862276250295fe60d10ee
lwr <a900240@gmail.com>
2023-09-13 11:07:57 -0700

    fix token

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -2,8 +2,7 @@ import React, {useState} from 'react';
 import axios from "axios";
 import sendIcon from "../send-svgrepo-com.svg"
 function PostForm(){
-    // const url="http://0.0.0.0:80/submit"
-    url=""
+    const url="http://0.0.0.0:80/submit"
 
     // const url="http://0.0.0.0:80/submit"
     // const [data, setData] = useState({
